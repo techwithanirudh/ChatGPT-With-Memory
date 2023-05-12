9061979986bc24503ee050065fa4d7f9bf460e0d
Anirudh <70373803+techwithanirudh@users.noreply.github.com>
2023-05-12 13:19:26 +0530

    Update firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -3,15 +3,7 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDrvSoRvOImIW1jdQeuf_cUFrjnM0O7icc",
-  authDomain: "anichatgpt.firebaseapp.com",
-  projectId: "anichatgpt",
-  storageBucket: "anichatgpt.appspot.com",
-  messagingSenderId: "58926213579",
-  appId: "1:58926213579:web:fa150a3d3c233825ceae4e",
-  measurementId: "G-BPRVV5YLZD",
-};
+const firebaseConfig = {};
 
 // Initialize Firebase
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
